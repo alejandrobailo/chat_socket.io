e2bdfed4979833d5864b4ab3b5bba7b8f79ba46a
Alejandro <alejandrobailo94@gmail.com>
2020-03-12 16:38:33 +0100

    Connect/disconnect, real time msg and design.

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', {
-    title: 'Express',
-    // msg: msg
+    title: 'Express'
   });
 });
 
